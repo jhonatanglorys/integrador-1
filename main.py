0b998d663cb2c6dc32451aec70344cbda391c809
jhontanglorys <jhonatanglorys@gmail.com>
2021-04-11 10:49:56 -0500

    busqueda e idioma por consola

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
 
+#Ingreso del parámetro de búsqueda
+cadena = input("Ingresa la cadena que deseas buscar: ")
+#Ingreso del idioma de búsqueda
+idioma = input("Ingresa ES para español, EN para inglés: ")
+#Reemplazo de espacios blancos por el signo +
+nueva_cadena = cadena.replace(" ", "+")
+
+
 #Cabecera del navegador
 headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'}
 #URL de acceso a los resultados de busqueda
-url = 'https://scholar.google.com/scholar?hl=es&as_sdt=0%2C10&q=web+scraping&btnG='
+url = 'https://scholar.google.com/scholar?hl='+idioma+'&q='+nueva_cadena
 
 #Respuesta obtenida
 response = requests.get(url, headers=headers)
