0980cfe50f4575409275d6a25988d63600e2d45a
jhontanglorys <jhonatanglorys@gmail.com>
2021-04-18 11:45:31 -0500

    crear y guardar en CSV

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import csv
 import requests
 
 #Ingreso del parámetro de búsqueda
@@ -8,6 +9,14 @@ idioma = input("Ingresa ES para español, EN para inglés: ")
 #Reemplazo de espacios blancos por el signo +
 nueva_cadena = cadena.replace(" ", "+")
 
+
+#Crear nuevo archivo
+file = open('resultados.csv','w')
+writer = csv.writer(file)
+
+
+#Escribir las cabeceras del archivo
+writer.writerow(['titulo','vinculo','resumen'])
 
 #Cabecera del navegador
 headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'}
@@ -23,9 +32,15 @@ soup= BeautifulSoup(response.content,'lxml')
 for item in soup.select('[data-lid]'):
     #print(item)
     #Titulo del articulo
-    print(item.select('h3')[0].get_text())
+    titulo = item.select('h3')[0].get_text()
+    print(titulo)
     #vinculo del articulo
-    print(item.select('a')[0]['href'])
+    vinculo = item.select('a')[0]['href']
+    print(vinculo)
     #Resumen
-    print(item.select('.gs_rs')[0].get_text())
+    resumen = item.select('.gs_rs')[0].get_text()
+    print(resumen)
     print('-------------------')
+    writer.writerow([titulo.encode('utf-8'),vinculo.encode('utf-8'),resumen.encode('utf-8')])
+
+file.close()
