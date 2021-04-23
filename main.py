92db4a45ec66ad53d594b452d87630fff68aac0e
juanjoser105 <juan.ramirez105@udea.edu.co>
2021-04-22 22:10:31 -0500

    Unificación GUI con busqueda, busqueda de más de 10 resultados

--- main.py
+++ main.py
@@ -1,46 +1,169 @@
 from bs4 import BeautifulSoup
 import csv
 import requests
+import tkinter as tk
+from tkinter import *
 
-#Ingreso del parámetro de búsqueda
-cadena = input("Ingresa la cadena que deseas buscar: ")
+
 #Ingreso del idioma de búsqueda
-idioma = input("Ingresa ES para español, EN para inglés: ")
-#Reemplazo de espacios blancos por el signo +
-nueva_cadena = cadena.replace(" ", "+")
+idioma = "ES"
+
+#Ingreso la cantidad que quiero de resultados (Multiplo de 10)
+cantidadres = 10
 
 
-#Crear nuevo archivo
-file = open('resultados.csv','w')
-writer = csv.writer(file)
+#Metodo para realizar la busqueda
+def buscarResultados(cadena, idioma, cantidadres):
+    #Ingreso del parámetro de búsqueda
+    subcadena = cadena.split(',')
+    cadena = subcadena[0]
+
+    #Reemplazo de espacios blancos por el signo +
+    nueva_cadena = cadena.replace(" ", "+")
 
 
-#Escribir las cabeceras del archivo
-writer.writerow(['titulo','vinculo','resumen'])
+    #Crear nuevo archivo
+    file = open('resultados.csv','w')
+    writer = csv.writer(file)
 
-#Cabecera del navegador
-headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'}
-#URL de acceso a los resultados de busqueda
-url = 'https://scholar.google.com/scholar?hl='+idioma+'&q='+nueva_cadena
 
-#Respuesta obtenida
-response = requests.get(url, headers=headers)
+    #Escribir las cabeceras del archivo
+    writer.writerow(['titulo','vinculo','resumen'])
 
-#creacion objeto BeaustifulSoup con los parametros requeridos para el webscraping
-soup= BeautifulSoup(response.content,'lxml')
+    #Cabecera del navegador
+    #headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'}
+    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36 Edg/89.0.774.77'}
 
-for item in soup.select('[data-lid]'):
-    #print(item)
-    #Titulo del articulo
-    titulo = item.select('h3')[0].get_text()
-    print(titulo)
-    #vinculo del articulo
-    vinculo = item.select('a')[0]['href']
-    print(vinculo)
-    #Resumen
-    resumen = item.select('.gs_rs')[0].get_text()
-    print(resumen)
-    print('-------------------')
-    writer.writerow([titulo.encode('utf-8'),vinculo.encode('utf-8'),resumen.encode('utf-8')])
+    #URL de acceso a los resultados de busqueda
+    url = 'https://scholar.google.com/scholar?hl='+idioma+'&q='+nueva_cadena
 
-file.close()
+    #Respuesta obtenida
+    response = requests.get(url, headers=headers)
+
+    #creacion objeto BeaustifulSoup con los parametros requeridos para el webscraping
+    soup= BeautifulSoup(response.content,'lxml')
+
+
+    itbusqueda = cantidadres/10
+    for i in range(0, int(itbusqueda)):
+        start = i*10
+        
+        #URL de acceso a los resultados de busqueda
+        url2 = 'https://scholar.google.com/scholar?hl='+idioma+'&q='+nueva_cadena+'&start='+str(start)
+        
+        #Respuesta obtenida
+        response = requests.get(url2, headers=headers)
+
+        #creacion objeto BeaustifulSoup con los parametros requeridos para el webscraping
+        soup= BeautifulSoup(response.content,'lxml')
+
+        for item in soup.select('[data-lid]'):
+            #print(item)
+            #Titulo del articulo
+            titulo = item.select('h3')[0].get_text()
+
+            tipodoc = titulo.find("[CITAS]")
+
+            if tipodoc==-1:
+                #vinculo del articulo
+                vinculo = item.select('a')[0]['href']
+                print(vinculo)
+                #Resumen
+                resumen = item.select('.gs_rs')[0].get_text()
+                print(resumen)
+                print('-------------------')
+                writer.writerow([titulo.encode('utf-8'),vinculo.encode('utf-8'),resumen.encode('utf-8')]) 
+            
+        itbusqueda = int(itbusqueda) - 1
+            
+    file.close()
+
+
+
+
+
+
+
+
+
+
+
+#Programa principal
+root = tk.Tk()
+root.title('Plan de búsqueda')
+root.iconbitmap('./images/pajaro.ico')
+root.geometry("850x400")
+
+lbl = Label(root, text="Plan de búsqueda", font=('Times', '30', 'italic'))
+lbl.pack()
+
+def button_click():
+    text = ''
+    cadena = edescr.get(1.0,END)
+    cadena2 = edistr.get(1.0,END)
+    separador = ","
+    vector = cadena.split(separador)
+    vector2 = cadena2.split(separador)
+    for ec in vector:
+        for ec2 in vector2:
+            text = text + ec.strip() + ' y ' + ec2.strip() +", "+ec.strip() + ' o ' + ec2.strip()+", "
+    
+    eexplog.insert(tk.END,text)  
+       
+            
+
+    
+    
+#Frame principal
+frame = tk.Frame(root, width=400, height=500)
+frame.pack(fill=None, expand=False)
+
+#Descriptores - 4,5
+lbldescr = Label(frame, text="Descriptores", font=('Times', '15', 'italic'))
+lbldescr.grid(row=0, column=0)
+edescr= Text(frame, width=30, height=5)
+edescr.grid(row=1, column=0, padx=10, pady=10)
+
+#Distractores - 4,5
+lbldistr = Label(frame, text="Distractores", font=('Times', '15', 'italic'))
+lbldistr.grid(row=0, column=1)
+edistr= Text(frame, width=30, height=5)
+edistr.grid(row=1, column=1, padx=10, pady=10)
+
+#Expresiones logicas de busqueda -6,7
+lblexplog = Label(frame, text="Expresiones logicas de busqueda", font=('Times', '13', 'italic'))
+lblexplog.grid(row=2, column=0)
+eexplog= Text(frame, width=65, height=5)
+eexplog.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
+
+#Boton Buscar,Limpiar - 6,7
+button_Buscar = Button(frame, text="Generar \n expresiones", command=button_click , padx=50, pady=5, font=('Times', '10', 'italic'))
+button_Buscar.grid(row=1, column=2)
+button_Buscar = Button(frame, text="Buscar", command=lambda: buscarResultados(eexplog.get(1.0,END),idioma,cantidadres),padx=50, pady=5, font=('Times', '15', 'italic'))
+button_Buscar.grid(row=2, column=2)
+button_Limpiar = Button(frame, text="Limpiar", padx=50, pady=5, font=('Times', '15', 'italic'))
+button_Limpiar.grid(row=3, column=2)
+
+
+root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
